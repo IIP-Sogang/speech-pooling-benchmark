91840ea4d8bae001cb2102d43e30710debe48fb8
Park323 <park32323@gmail.com>
2023-01-31 17:00:24 +0900

    Fix scheduler

--- scheduler/steplr.py
+++ scheduler/steplr.py
@@ -2,11 +2,16 @@
 # -*- encoding: utf-8 -*-
 
 import torch
+from torch.optim.lr_scheduler import StepLR
+from optimizer.adam import Optimizer
 
 def Scheduler(optimizer, step_size, gamma, **kwargs):
-    sche_fn = torch.optim.lr_scheduler.StepLR(optimizer, step_size=step_size, gamma=gamma)
-
-
-    print('Initialised step LR scheduler')
-
+    sche_fn = StepLRScheduler(optimizer, step_size=step_size, gamma=gamma, last_epoch=kwargs.get('last_epoch', -1))
+    print('Initialized step LR scheduler')
     return sche_fn
+
+class StepLRScheduler(StepLR):
+    def __init__(self, optimizer: Optimizer, step_size: int, gamma: float = ..., last_epoch: int = ...) -> None:
+        super().__init__(optimizer, step_size, gamma, last_epoch)
+        self.interval = 'epoch'
+        self.frequency = 1
