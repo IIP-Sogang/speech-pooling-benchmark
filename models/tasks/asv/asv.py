ad2deec3d07f030a62ed5eee1230eb5cead8fb29
Park323 <park32323@gmail.com>
2023-02-06 14:09:10 +0900

    Update filename

--- models/tasks/asv/asv.py
+++ models/tasks/asv/asv.py
@@ -5,23 +5,19 @@ import torch.nn as nn
 from torch import Tensor
 import torch.nn.functional as F
 
-from models.head import select_head
+from models.aggregate import select_head
 from models.modules import SimpleLinear
 from models.tasks.absract import TaskDependentModule
-
-from x_vector import X_vector
 
 
 class SpeakerVerificationModule(TaskDependentModule):
     def __init__(self, input_dim:int = 768, head_type='avgpool', **kwargs) -> None:
         super().__init__()
-        self.x_vector_extractor = X_vector(input_dim=input_dim)
         self.head = select_head(head_type, **kwargs)
         self.linear = SimpleLinear(512, input_dim)
 
     def forward(self, inputs: Tensor) -> Tensor:
-        x_vector = self.x_vector_extractor(inputs)
-        speech_representation = self.head(x_vector)
+        speech_representation = self.head(inputs)
         outputs = self.linear(speech_representation)
         return outputs
 
